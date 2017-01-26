40c8525d9446574b7e03bdca66873187dd37f258
dil3mm4 <francoismanzo@gmail.com>
2017-01-26 15:37:38 +0100

    Update to match luv80ds specs

--- aosp.mk
+++ aosp.mk
@@ -1,22 +1,22 @@
 # Release name
-PRODUCT_RELEASE_NAME := q380
+PRODUCT_RELEASE_NAME := luv80ds
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/micromax/q380/device_q380.mk)
+$(call inherit-product, device/lge/luv80ds/device_luv80ds.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := q380
-PRODUCT_NAME := aosp_q380
-PRODUCT_BRAND := micromax
-PRODUCT_MODEL := q380
-PRODUCT_MANUFACTURER := micromax
+PRODUCT_DEVICE := luv80ds
+PRODUCT_NAME := aosp_luv80ds
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := d331
+PRODUCT_MANUFACTURER := lge
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-mediatek
