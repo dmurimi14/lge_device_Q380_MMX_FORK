ae012022570556855138db39f2a89a2658464949
dil3mm4 <francoismanzo@gmail.com>
2017-01-26 15:39:59 +0100

    Update to match luv80ds specs

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := q380
+PRODUCT_RELEASE_NAME := luv80ds
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -8,18 +8,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/micromax/q380/device_q380.mk)
+$(call inherit-product, device/lge/luv80ds/device_q380.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := q380
-PRODUCT_NAME := cm_q380
-PRODUCT_BRAND := micromax
-PRODUCT_MODEL := q380
-PRODUCT_MANUFACTURER := micromax
+PRODUCT_DEVICE := luv80ds
+PRODUCT_NAME := cm_luv80ds
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := d331
+PRODUCT_MANUFACTURER := lge
 
 PRODUCT_GMS_CLIENTID_BASE := android-mediatek
 
